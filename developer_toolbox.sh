ef2612d01703e8806b394c4bc5902fbdc40bfaab
Ron Mulero <ronaldmulero@me.com>
2015-02-03 15:55:49 -0500

    Add variablecheck module.

--- developer_toolbox.sh
+++ developer_toolbox.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-drush dl admin_menu advanced_help coder devel devel_themer diff drupalforfirebug environment_indicator examples features features_extra features_override field_inspector ftools hacked modules_weight profiler profiler_builder profiler_example reroute_email schema simplehtmldom stage_file_proxy util --destination="sites/all/modules/developer_toolbox" -y;
+drush dl admin_menu advanced_help coder devel devel_themer diff drupalforfirebug environment_indicator examples features features_extra features_override field_inspector ftools hacked modules_weight profiler profiler_builder profiler_example reroute_email schema simplehtmldom stage_file_proxy util variablecheck --destination="sites/all/modules/developer_toolbox" -y;
